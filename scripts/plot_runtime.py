59f4d6043d232ad8aca5017b450c90ea71ec9b08
Matthias Wolf <matthias.wolf@cern.ch>
2016-08-10 15:33:35 +0200

    Work on labels.

--- scripts/plot_runtime.py
+++ scripts/plot_runtime.py
@@ -1,6 +1,9 @@
 import argparse
 import json
+import matplotlib
 import matplotlib.pylab as plt
+
+matplotlib.rc('axes.formatter', limits=(-3, 4))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input")
@@ -12,14 +15,19 @@ with open(args.input) as fd:
     edges = [d['key'] for d in data["aggregations"]["runtime"]["runtime_inner"]["buckets"]]
     bins = [d['doc_count'] for d in data["aggregations"]["runtime"]["runtime_inner"]["buckets"]]
 
-ax = plt.subplot(311)
-ax.plot(edges, bins)
-ax.set_yscale("log", nonposy='clip')
-ax = plt.subplot(312)
-ax.plot(edges, bins)
-ax.set_xlim(0, 180)
-ax = plt.subplot(313)
-ax.plot(edges[60:], bins[60:])
-ax.set_xlim(0, 180)
+ax1 = plt.subplot(311)
+ax1.plot(edges, bins)
+ax1.set_yscale("log", nonposy='clip')
+ax1.set_ylabel("# jobs")
+ax2 = plt.subplot(312)
+ax2.plot(edges, bins)
+ax2.set_xlim(0, 180)
+ax2.set_ylabel("# jobs")
+plt.setp(ax2.get_xticklabels(), visible=False)
+ax3 = plt.subplot(313, sharex=ax2)
+ax3.plot(edges[60:], bins[60:])
+ax3.set_xlabel("runtime")
+ax3.set_xlim(0, 180)
+ax3.set_ylabel("# jobs > 1h")
 
 plt.savefig(args.output)
